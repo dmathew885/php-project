e69db30abcb95b4fb017fd9eb090825da8a6f532
don mathew <dmathew885@gmail.com>
2021-10-31 17:31:18 -0400

    final

--- store.php
+++ store.php
@@ -1,10 +1,19 @@
-<h1>   
+<head>
+    <style>
+        h1 {text-align: center;}
+        div {text-align: center;}
+         </style> 
+ <h1>   
 booKKart
 </h1>
-<span>
- <a href="index.html">home</a>
- <a href="store.php">store</a>    
-</span>
+<div class="topnav">
+       <a href="index.html">Home</a>
+       <a href="store.php">Store</a>  
+      </div>
+</head>
+ 
+<body>
+  
 <h2>Please Click on a Book you like to Buy!!!</h2>
 <?php
     session_start();
